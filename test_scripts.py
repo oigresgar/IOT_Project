ac7e9501db1be6e657c3a7f10d5b91caf89f5d30
oigresgar <oigresgar@gmail.com>
2025-03-03 18:04:08 +0100

    Get y Post inicial

---
+++ test_scripts.py
@@ -0,0 +1,58 @@
+import requests
+
+
+class AmiLabHttp:
+    def __init__(self, url: str, token: str):
+        self.url = url
+        self.token = token
+
+    def get_state(self, entity_id: str):
+        headers = {
+            "Authorization": "Bearer " + self.token,
+            "Content-Type": "application/json",
+        }
+        return requests.get(self.url + "/states/" + entity_id, headers=headers).json()
+
+    def post_service(
+        self, entity_id: str, service: str, command: str, extra_data: dict = {}
+    ):
+        headers = {
+            "Authorization": "Bearer " + self.token,
+            "Content-Type": "application/json",
+        }
+        data = {
+            "entity_id": entity_id,
+        }
+        data.update(extra_data)
+        return requests.post(
+            self.url + "/services/" + service + "/" + command,
+            headers=headers,
+            json=data,
+        ).json()
+    
+    def get_snapshot(self) -> bytes:
+        headers = {
+            "Authorization": "Bearer " + self.token,
+            "Content-Type": "image/jpeg",
+        }
+        return requests.get(self.url + "/camera_proxy/camera.amicam", headers=headers).content
+        
+
+
+if __name__ == "__main__":
+    url = "https://danubio.ii.uam.es/api"
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NDdlODRkYTExNmI0Yzk2YjIxM2ExMDdhMTY2NmVmMCIsImlhdCI6MTY3ODI3MTU4OSwiZXhwIjoxOTkzNjMxNTg5fQ.-jUvyVsmzQMxrkRd1kLNM_PIH1HTGnzsOzESDWlPukE"
+    ami_lab = AmiLabHttp(url, token)
+    # print(ami_lab.get_state(entity_id="light.lampara_de_lectura"))
+    # print(
+    #     ami_lab.post_service(
+    #         entity_id="light.lampara_derecha",
+    #         service="light",
+    #         command="turn_on",
+    #         extra_data={"brightness_pct": "100", "rgb_color": [255, 0, 0]},
+    #     )
+    # )
+
+    img_bytes = ami_lab.get_snapshot()
+    with open("snapshot.jpeg", "wb") as f:
+        f.write(img_bytes)
