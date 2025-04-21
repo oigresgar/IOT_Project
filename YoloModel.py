3ef84439be3588a7817608ebcce0acccc6a78ac3
Enrique Ortega Yegros <39568611+Kijuoye@users.noreply.github.com>
2025-04-21 18:24:11 +0200

    fix(YoloModel): missing red color on image conversion

--- YoloModel.py
+++ YoloModel.py
@@ -4,14 +4,13 @@ from PIL import Image
 
 YOLO_MODEL_PATH = "yolo11n.pt"
 
+
 class YoloModel:
-    def __init__(self, model_path = YOLO_MODEL_PATH):
+    def __init__(self, model_path=YOLO_MODEL_PATH):
         self.model = YOLO(model_path)
-    
 
     def count_people_in_img(self, source: Image.Image) -> tuple[int, Image.Image]:
         results = self.model.predict(source=source, classes=[0])
-        results[0].save("output.jpg")
-        plot_img = results[0].plot(pil=True)
+        results[0].save("annotated.jpg")
+        plot_img = Image.open("annotated.jpg")
         return len(results[0].boxes), plot_img
-    
